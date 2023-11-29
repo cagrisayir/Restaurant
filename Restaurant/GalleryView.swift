1fca99ba95ec19c98d61bddbdd969cca4b8e7b49
Omer Cagri Sayir <omercagris@gmail.com>
2023-11-29 09:52:42 +0300

    Added Menu View to the project

---
+++ Restaurant/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  Restaurant
+//
+//  Created by Omer Cagri Sayir on 29.11.2023.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryView()
+}
---
+++ Restaurant/MenuView.swift
@@ -0,0 +1,31 @@
+//
+//  MenuView.swift
+//  Restaurant
+//
+//  Created by Omer Cagri Sayir on 29.11.2023.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    
+    @State var menuItems: [MenuItem] = [MenuItem]()
+    var dataService = DataService()
+    
+    var body: some View {
+        
+        List(menuItems) { item in
+            MenuListRow(item: item)
+          
+        }
+        .listStyle(.plain)
+        .onAppear{
+            menuItems = dataService.getData()
+        }
+        
+    }
+}
+
+#Preview {
+    MenuView()
+}
